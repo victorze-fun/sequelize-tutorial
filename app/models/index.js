c85d0ab840ce355554b8d2a7098959499394ec38
Victor <victorze@protonmail.com>
2021-09-21 15:12:01 -0500

    Add config sequelize

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+
+// pgcli -h localhost -p 5432 -U username postgres
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION
+  }
+);
+
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
